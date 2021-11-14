9282091787821e9b5eec2a555b3fd4f50d36bfa7
ulusansoftware <ulusansoftware@outlook.com>
2021-11-14 10:59:14 +0300

    JWT and Encryption Added

---
+++ Core/Utilities/Security/JWT/ITokenHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities.Entities;
+
+namespace Core.Utilities.Security.JWT
+{
+    public interface ITokenHelper
+    {
+        //kullanıcı adını ve şifresini yazdıktan sonra buraya gelir.
+        //sonra createtoken fonksiyonu çalışır ve doğruysa jwt üretir ve kullanıcıya verir.
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
+    }
+}
